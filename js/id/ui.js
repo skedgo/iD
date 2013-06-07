09e067842fbdba22267c90a20194420ec6d9df3a
Jayen Ashar <jayen@buzzhives.com>
2013-06-07 17:25:14 +1000

    adding iD module for intellij changing default location for iD

--- js/id/ui.js
+++ js/id/ui.js
@@ -10,7 +10,7 @@ iD.ui = function(context) {
         hash();
 
         if (!hash.hadHash) {
-            map.centerZoom([-77.02271, 38.90085], 20);
+            map.centerZoom([151.2077, -33.8605], 16);
         }
 
         container.append('div')
